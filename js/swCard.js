7ea952a920b394ad036a1ba3ee6982402ef14559
yeonhuij844 <yeonhuij844@gmail.com>
2025-06-05 12:15:34 +0900

    2025-06-05

--- js/swCard.js
+++ js/swCard.js
@@ -1,8 +1,30 @@
 const swCard = new Swiper(".sw-card", {
+  centeredSlides: false,
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //반응형
+  breakpoints: {
+    320: {
+      centeredSlides: true,
+      spaceBetween: 20,
+    },
+    768: {
+      centeredSlides: false,
+      spaceBetween: 30,
+    },
+    1220: {
+      centeredSlides: false,
+      spaceBetween: 30,
+    },
   },
 });
