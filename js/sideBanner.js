9e76b092e8dcb5d27844590da2d6be8532b5ef6e
yeonhuij844 <yeonhuij844@gmail.com>
2025-06-10 12:10:09 +0900

    반응형

--- js/sideBanner.js
+++ js/sideBanner.js
@@ -1,8 +1,15 @@
 const SideBannerList = document.querySelectorAll(
   ".side-banner-wrap .side-banner"
 );
+const sideBannerListMb = document.querySelectorAll(
+  ".side-banner-wrap-mb .side-banner"
+);
 const bgColorList = ["#edf4ff", "#def9fe", "#fff6de"];
 
 SideBannerList.forEach((sideBanner, index) => {
+  sideBanner.style.backgroundColor = bgColorList[index];
+});
+
+sideBannerListMb.forEach((sideBanner, index) => {
   sideBanner.style.backgroundColor = bgColorList[index];
 });
